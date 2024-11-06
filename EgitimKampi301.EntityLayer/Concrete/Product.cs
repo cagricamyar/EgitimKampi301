d0901e628e72d4f4b5b0d8cb81be9888ff166b88
çağrı çamyar <cagri-95@hotmail.com>
2024-11-06 17:55:20 +0300

    Ders12 Sonu

--- EgitimKampi301.EntityLayer/Concrete/Product.cs
+++ EgitimKampi301.EntityLayer/Concrete/Product.cs
@@ -13,6 +13,8 @@ namespace EgitimKampi301.EntityLayer.Concrete
         public int UnitsInStock { get; set; }
         public decimal ProductPrice { get; set; }
         public string ProductDescription { get; set; }
-
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        public List<Order> Orders { get; set; }
     }
 }
